0da2983eea43d56803b40d96eeb050857890c41e
Gaurav Chand <46017862+itsmegc@users.noreply.github.com>
2018-12-21 17:58:10 +0530

    Add files via upload

---
+++ Pallindrom/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter any number");
+        int num = Convert.ToInt32(Console.ReadLine());  //1234
+        int temp = num;
+        int rev = 0, rem = 0;
+        while (num > 0)  //1234, 123, 12, 1
+        {
+            rem = num % 10;  //1234%10=4, 123%10=3, 12%10=2, 1%10=1
+            rev = rev * 10 + rem; //0+4=4, 4*10+3=43, 43*10+2=432, 432*10+1=4321
+            num = num / 10; //1234/10=123, 123/10=12, 12/10=1, 1/10=0
+        }
+        if (temp == rev)
+        {
+            Console.WriteLine("Pallindrom number");
+        }
+        else
+        {
+            Console.WriteLine("Not pallindrom number");
+        }
+        Console.ReadKey();
+    }
+}
+
